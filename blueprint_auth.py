5092acd4f63e00d7805609818e498bd1fc5fa908
KarthikeyanRanasthala <karthikeyan.ranasthala@gmail.com>
2019-12-14 21:44:45 +0530

    Implement Authentication

--- blueprint_auth.py
+++ blueprint_auth.py
@@ -1,13 +1,46 @@
-from flask import Blueprint, request, Response
+from flask import Blueprint, request, Response, jsonify
+from utils import (
+    validate_user_input,
+    generate_salt,
+    generate_hash,
+    db_write,
+    validate_user,
+)
 
 authentication = Blueprint("authentication", __name__)
 
 
 @authentication.route("/register", methods=["POST"])
 def register_user():
-    pass
+    user_email = request.json["email"]
+    user_password = request.json["password"]
+    user_confirm_password = request.json["confirm_password"]
+
+    if user_password == user_confirm_password and validate_user_input(
+        "authentication", email=user_email, password=user_password
+    ):
+        password_salt = generate_salt()
+        password_hash = generate_hash(user_password, password_salt)
+
+        if db_write(
+            """INSERT INTO users VALUES (%s, %s, %s)""",
+            (user_email, password_salt, password_hash),
+        ):
+            return Response(status=201)
+        else:
+            return Response(status=409)
+    else:
+        return Response(status=400)
 
 
 @authentication.route("/login", methods=["POST"])
 def login_user():
-    pass
+    user_email = request.json["email"]
+    user_password = request.json["password"]
+
+    user_token = validate_user(user_email, user_password)
+
+    if user_token:
+        return jsonify({"jwt_token": user_token})
+    else:
+        Response(status=401)
--- utils.py
+++ utils.py
@@ -5,23 +5,82 @@ from hashlib import pbkdf2_hmac
 from app import db
 
 import os
+import jwt
 
 
 def db_read(query, params=None):
-    pass
+    cursor = db.connection.cursor()
+    if params:
+        cursor.execute(query, params)
+    else:
+        cursor.execute(query)
+
+    entries = cursor.fetchall()
+    cursor.close()
+
+    content = []
+
+    for entry in entries:
+        content.append(entry)
+
+    return content
 
 
 def db_write(query, params):
-    pass
+    cursor = db.connection.cursor()
+    try:
+        cursor.execute(query, params)
+        db.connection.commit()
+        cursor.close()
+
+        return True
+
+    except MySQLdb._exceptions.IntegrityError:
+        cursor.close()
+        return False
 
 
 def generate_salt():
-    pass
+    salt = os.urandom(16)
+    return salt.hex()
 
 
 def generate_hash(plain_password, password_salt):
-    pass
+    password_hash = pbkdf2_hmac(
+        "sha256",
+        b"%b" % bytes(plain_password, "utf-8"),
+        b"%b" % bytes(password_salt, "utf-8"),
+        10000,
+    )
+    return password_hash.hex()
+
+
+def generate_jwt_token(content):
+    encoded_content = jwt.encode(content, JWT_SECRET_KEY, algorithm="HS256")
+    token = str(encoded_content).split("'")[1]
+    return token
+
+
+def validate_user_input(input_type, **kwargs):
+    if input_type == "authentication":
+        if len(kwargs["email"]) <= 255 and len(kwargs["password"]) <= 255:
+            return True
+        else:
+            return False
 
 
 def validate_user(email, password):
-    pass
+    current_user = db_read("""SELECT * FROM users WHERE email = %s""", (email,))
+
+    if len(current_user) == 1:
+        saved_password_hash = current_user[0]["password_hash"]
+        saved_password_salt = current_user[0]["password_salt"]
+        password_hash = generate_hash(password, saved_password_salt)
+
+        if password_hash == saved_password_hash:
+            user_id = current_user[0]["id"]
+            jwt_token = generate_jwt_token({"id": user_id})
+            return jwt_token
+
+    else:
+        return False
